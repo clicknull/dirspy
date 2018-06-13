9abeb3f7ebda1394203b2c125e805f908046a2e8
koboi137 <budikomarudin1337@gmail.com>
2018-06-14 05:48:06 +0700

    Update version

--- dirspy.py
+++ dirspy.py
@@ -5,8 +5,9 @@
 from datetime import datetime
 from threading import Thread
 from time import sleep, strftime
-import requests, sys
+import requests, sys, urllib3
 
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 file = open('dirs.txt', 'r').read().split('\n')
 user_agent = {'User-agent': 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)'}
 
@@ -22,9 +23,9 @@ Backbox Indonesia (c) 2017 - 2018
 """
 
 def helep():
-	print "DirsPY v1.2 ( www.backboxindonesia.or.id )"
-	print "Usage : python2 dirspy.py <url>"
-	print "EXAMPLE : python2 dirspy.py http://127.0.0.1/"
+	print('DirsPY v1.2 ( www.backboxindonesia.or.id )')
+	print('Usage : python2 dirspy.py <url>')
+	print('EXAMPLE : python2 dirspy.py http://127.0.0.1/')
 
 class cl:
 	pink = '\033[95m'
@@ -38,7 +39,7 @@ class cl:
 
 def rikues(line):
 	alamat = str(url) + str(line)
-	try: r = requests.get(alamat, headers = user_agent, timeout = 5)
+	try: r = requests.get(alamat, headers = user_agent, timeout = 5, verify=False)
 	except (requests.ConnectionError, requests.exceptions.Timeout, requests.exceptions.ChunkedEncodingError, IOError): sys.exit()
 	status = r.status_code
 
